2bef2b0d2eb6786f0978ae1b15a3bd1f513b9117
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-07-07 16:21:32 -0300

    Implemented the first unit tests.

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,37 @@
+package com.healthycoderapp;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BMICalculatorTest {
+    @Test
+    public void should_ReturnTrue_When_DietRecommended(){
+        // given
+        double weight = 89.0;
+        double height = 1.72;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+
+    @Test
+    public void should_ReturnFalse_When_DietNotRecommended(){
+        // given
+        double weight = 50.0;
+        double height = 1.92;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertFalse(recommended);
+    }
+}
